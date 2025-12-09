488c0607c3b074da2596ddb1c714e5c9b4b05c2f
Prithvi Thakur <prithvi@Prithvis-MacBook-Pro-3.local>
2025-12-09 07:34:09 -0500

    remove lsp config deprecation warning

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,16 +1,30 @@
+-- hayloft LSP setup 🐑
+
+-- 1. Mason plugin setup
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "pyright", "rust_analyzer", "julials", "clangd" },
+  automatic_installation = true,
+})
 
-require("lspconfig").pyright.setup{}
-require("lspconfig").rust_analyzer.setup{}
-require("lspconfig").julials.setup{}
-require("lspconfig").clangd.setup{}
+-- 2. General LSP capabilities
+local lspconfig = require("lspconfig")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
--- Autoformat on save with clang-format 🪚
-vim.api.nvim_create_autocmd('BufWritePre', {
-    pattern = { '*.cpp', '*.h', '*.cu' },
-    callback = function()
-        vim.cmd('silent! !clang-format -i %')
-    end
+-- 3. Setup each language server explicitly
+local servers = { "pyright", "rust_analyzer", "julials", "clangd" }
+
+for _, server in ipairs(servers) do
+  lspconfig[server].setup({
+    capabilities = capabilities,
+  })
+end
+
+-- 4. Autoformat on save with clang-format 🪚
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.cpp", "*.h", "*.cu" },
+  callback = function()
+    vim.cmd("silent! !clang-format -i %")
+  end,
 })
 
